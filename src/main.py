85b306632d60bc095c8808c0a3a1d99d76e121c2
Beck Iverson <Beckiverson2@gmail.com>
2021-11-15 18:17:28 -0800

    Project is broken AF, not copying issue, fix JINJA

---
+++ src/main.py
@@ -0,0 +1,29 @@
+from typing import Dict, List
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqLite:///myths.db'
+db = SQLAlchemy(app)
+
+
+class Myth(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    url = db.Column(db.String)
+
+
+db.create_all()
+
+
+@app.route('/')
+def index():
+    myths: List[Dict[str, str]] = [
+        {'title': 'Birds are real', 'url': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'},
+        {'title': 'Birds are fake', 'url': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}
+    ]
+    return render_template('Index.html', myths=myths)
+
+
+app.run(debug=True)
