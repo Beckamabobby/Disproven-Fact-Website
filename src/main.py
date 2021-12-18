a926e414cdb86bbb9896fccb4678f9ca6a81bf06
Beck Iverson <Beckiverson2@gmail.com>
2021-12-17 17:38:02 -0800

    Add JSON stuff, add new functions, add fetch functionality.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -16,16 +16,24 @@ class Myth(db.Model):
 
 db.create_all()
 
+myths_list: List[Dict[str, str]] = [
+    {'title': 'Birds are real', 'synopsis': 'Government literally 1984',
+        'url': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'startyear': '1970', 'endyear': '1990'},
+    {'title': 'Birds are fake', 'synopsis': 'Are you stupid',
+        'url': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'startyear': '1972', 'endyear': '1984'}
+]
 
 @app.route('/')
 def index():
-    myths: List[Dict[str, str]] = [
-        {'title': 'Birds are real', 'synopsis': 'Government literally 1984',
-            'url': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'startyear': '1970', 'endyear': '1990'},
-        {'title': 'Birds are fake', 'synopsis': 'Are you stupid',
-            'url': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'startyear': '1972', 'endyear': '1984'}
-    ]
-    return render_template('index.html', myths=myths)
+    return render_template('index.html', myths=myths_list)
+
+@app.route('/myths/<graduation_year>')
+def myths(graduation_year):
+    print(graduation_year)
+    # TODO filter by graduation year
+    return jsonify(myths_list)
+
+#ooga booga
 
 
 app.run(debug=True)
