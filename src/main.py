b72232137188cd478cd52985ab29767a34a416f4
Beck Iverson <Beckiverson2@gmail.com>
2022-03-18 16:00:19 -0700

    Create list comprehension for pulling from database. Make myths_list have str and int as value types. Get rid of old myths_list.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from typing import Dict, List
-from random import choice
 
 from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -9,6 +8,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myths.db'
 db = SQLAlchemy(app)
 
 
+user_myth = db.Table('user_myth',
+                     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+                     db.Column('myth_id', db.Integer, db.ForeignKey('myth.id'), primary_key=True)
+                     )
+
 class Myth(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -17,27 +21,31 @@ class Myth(db.Model):
     startyear = db.Column(db.Integer, nullable=False)
     endyear = db.Column(db.Integer, nullable=False)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+
+# class UserMyth(db.Model):
+#     id      = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+#     myth_id = db.Column(db.Integer, db.ForeignKey('page.id'), primary_key=True)
 
 db.create_all()
 
-myths_db = Myth.query.all()
-
-
-myths_list: List[Dict[str, str]] = [
-    {'title': 'Birds are real', 'synopsis': 'Government literally 1984',
-        'url': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'startyear': '1970', 'endyear': '1990'},
-    {'title': 'Birds are fake', 'synopsis': 'Are you stupid',
-        'url': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'startyear': '1972', 'endyear': '1983'},
-    {'title': 'Birds are cringe', 'synopsis': 'Are you sussy',
-        'url': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'startyear': '1979', 'endyear': '1994'},
-]
+myths = Myth.query.all()
+myths_list: List[Dict[str, str | int]] = [{
+    'title': myth.title,
+    'synopsis': myth.synopsis,
+    'url': myth.url,
+    'startyear': int(myth.startyear),
+    'endyear': int(myth.endyear)} for myth in myths]
 
 @app.route('/')
 def index():
     return render_template('index.html', myths=myths_list, years=range(1970, 1995))
 
-@app.route('/myths/<graduation_year>')
-def myths(graduation_year):
+@app.route('/myths/<int:graduation_year>')
+def myths(graduation_year: int):
     print(graduation_year)
     selected_myths = [myth for myth in myths_list if myth['startyear'] <= graduation_year <= myth['endyear']]
     return jsonify(selected_myths)
